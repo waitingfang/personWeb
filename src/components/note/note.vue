9b5a4425ca27b257c6e321872b8f71da3f1dd14c
waitingfang <waitingg1021@163.com>
2018-11-13 11:45:24 +0800

    modify data

--- src/components/note/note.vue
+++ src/components/note/note.vue
@@ -3,13 +3,17 @@
 		<div class="notewrapper">
 			<div class="notelist" v-for="(note, index) in notes" :key="index">
 				<div class="title">
-					<h2>{{note.title}}</h2>
+					<h2 class="name">{{note.title}}</h2>
+					<p class="date">{{note.date}}</p>
 				</div>
 				<div class="content">
-					<p>{{notes.content}}</p>
+					<p>{{note.content}}</p>
 				</div>
-				<div class="date">{{notes.date}}</div>
 			</div>
+		</div>
+		<div class="bottom">
+			<p>友情链接友情链接友情链接友情链接友情链接友情链接友情链接友情链接</p>
+			<p>友情链接友情链接友情链接友情链接友情链接友</p>
 		</div>
 	</div>
 </template>
@@ -35,23 +39,49 @@ export default {
 </script>
 
 <style scoped>
-.note {
-	width: 80%;
-	margin: 0 auto;
-}
 .notelist {
-	width: 100%;
+	position: relative;
+	margin: 0 auto;
+	width: 80%;
+	font-family: -apple-system,"Helvetica Neue",Arial,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","WenQuanYi Micro Hei",FontAwesome,sans-serif !important;
+	color: #565656;
 }
 .notelist .title {
+	position: relative;
 	width: 100%;
 	height: 40px;
+	line-height: 40px;
 	background: #eee;
 	color: #333;
 }
+.notelist .title .name {
+	padding-left: 10px;
+	color: #333;
+}
+.notelist .title .date {
+	position: absolute;
+	right: 10px;
+	bottom: 0px;
+	font-size: 10px;
+	color: #333;
+}
 .notelist .content {
+	margin: 5px auto;
+	padding: 15px;
+	font-size: 12px;
+	line-height: 22px;
+	text-indent: 2em;
+}
+.bottom{
+	background: #282828;
 	margin-top: 10px;
-	padding: 10px;
-	font-size: 16px;
-	color: #eee;
+	width: 100%;
+	height: 40px;
+	text-align: center;
+	border-top: 2px solid #ccc;
+	padding-top:5px;
+	font-size: 10px;
+	line-height: 18px;
+	color: #fff;
 }
 </style>
