85979edfc44c05ac2719252f51ef02ee7d7c3351
waitingfang <waitingg1021@163.com>
2018-11-13 11:04:45 +0800

    add data

--- src/components/note/note.vue
+++ src/components/note/note.vue
@@ -2,9 +2,13 @@
 	<div class="note">
 		<div class="notewrapper">
 			<div class="notelist" v-for="(note, index) in notes" :key="index">
-				<div class="title">{{note.title}}</div>
-				<div class="content"></div>
-				<div class="date"></div>
+				<div class="title">
+					<h2>{{note.title}}</h2>
+				</div>
+				<div class="content">
+					<p>{{notes.content}}</p>
+				</div>
+				<div class="date">{{notes.date}}</div>
 			</div>
 		</div>
 	</div>
@@ -34,5 +38,20 @@ export default {
 .note {
 	width: 80%;
 	margin: 0 auto;
+}
+.notelist {
+	width: 100%;
+}
+.notelist .title {
+	width: 100%;
+	height: 40px;
+	background: #eee;
+	color: #333;
+}
+.notelist .content {
+	margin-top: 10px;
+	padding: 10px;
+	font-size: 16px;
+	color: #eee;
 }
 </style>
